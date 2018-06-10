eee19a4826483ff162d2977a8935934bc2ff796e
Medill-East <NEMailLHD@163.com>
2018-06-10 14:34:34 +0800

    Week1 finished
    20180610 1434

---
+++ Introduction_to_C#Programming_and_Unity/Week1/ItsAllGreektoMe/ItsAllGreektoMe/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ItsAllGreektoMe
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double x1, y1, x2, y2;
+            Function function = new Function();
+
+            Console.WriteLine("Welcome! You could enter the coordinates of two points and get the distance and angle between them!");
+            Console.WriteLine("Point 1 X :");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Point 1 Y :");
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Point 2 X :");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Point 2 X :");
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+            function.CalculateDistance(x1, y1, x2, y2);
+            function.CalculateAngle(x1, y1, x2, y2);
+        }
+    }
+
+
+    class Function
+    {
+        public double CalculateDistance(double x1, double y1, double x2, double y2)
+        {
+            double xDistance = Math.Pow(x2 - x1, 2);
+            double yDistance = Math.Pow(y2 - y1, 2);
+            double finalDistance = Math.Sqrt(xDistance + yDistance);
+            Console.WriteLine("Distance between points:" + finalDistance);
+            return finalDistance;
+        }
+
+        public double CalculateAngle(double x1,double y1,double x2,double y2)
+        {
+            double angle = Math.Atan2((y2 - y1), (x2 - x1)) * 180 / Math.PI;
+            Console.WriteLine("Angle between points:" + angle);
+            return angle;
+        }
+
+
+    }
+}
