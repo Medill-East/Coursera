9f2b891da9d52a52ca089054584a3c99cef0651d
Medill-East <NEMailLHD@163.com>
2018-07-24 14:56:06 +0800

    Rock of Ages
    201807241450

---
+++ Introduction_to_C#Programming_and_Unity/Week3/Rock of Ages/Assets/Scripts/Rock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour {
+
+    //SpriteRenderer spriteRenderer;
+    //Object[] rocks;
+
+	// Use this for initialization
+	void Start () {
+        //spriteRenderer = this.transform.GetComponent<SpriteRenderer>();
+
+        //rocks = Resources.LoadAll("Sprites",typeof(Sprite));
+
+        //Sprite sprite = (Sprite)rocks[Random.Range(0, rocks.Length)];
+        //spriteRenderer.sprite = sprite;
+
+        // apply impulse force to get game object moving
+        const float MinImpulseForce = 1f;
+        const float MaxImpulseForce = 2f;
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        Vector2 direction = new Vector2(
+            Mathf.Cos(angle), Mathf.Sin(angle));
+        float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+        GetComponent<Rigidbody2D>().AddForce(
+            direction * magnitude,
+            ForceMode2D.Impulse);
+	}
+
+    private void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Introduction_to_C#Programming_and_Unity/Week3/Rock of Ages/Assets/Scripts/RockSpawner.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSpawner : MonoBehaviour {
+
+    [SerializeField]
+    GameObject prefabRock;
+
+    Object[] rocks;
+    public int rockNumber = 0;
+
+    const float MinSpawnDelay = 1;
+    const float MaxSpawnDelay = 2;
+    Timer spawnTimer;
+
+    //const int SpawnBorderSize = 100;
+    //int minSpawnX;
+    //int maxSpawnX;
+    //int minSpawnY;
+    //int maxSpawnY;
+
+    private void Start()
+    {
+        //minSpawnX = SpawnBorderSize;
+        //maxSpawnX = Screen.width - SpawnBorderSize;
+        //minSpawnY = SpawnBorderSize;
+        //maxSpawnY = Screen.height - SpawnBorderSize;
+
+        spawnTimer = gameObject.AddComponent<Timer>();
+        spawnTimer.Duration = Random.Range(MinSpawnDelay, MaxSpawnDelay);
+        spawnTimer.Run();
+    }
+
+    private void Update()
+    {
+        if(spawnTimer.Finished)
+        {
+            SpawnRock();
+
+            spawnTimer.Duration = Random.Range(MinSpawnDelay, MaxSpawnDelay);
+            spawnTimer.Run();
+        }
+    }
+
+    void SpawnRock()
+    {
+        //Vector3 location = new Vector3(Random.Range(minSpawnX, maxSpawnX),
+                                      //Random.Range(minSpawnY, maxSpawnY),
+                                       //-Camera.main.transform.position.z);
+        Vector3 location = new Vector3(Screen.width/2,
+                              Screen.height/2,
+                               -Camera.main.transform.position.z);
+        Vector3 worldLocation = Camera.main.ScreenToWorldPoint(location);
+
+        if(GameObject.FindGameObjectsWithTag("Rock").Length < rockNumber)
+        {
+            GameObject rock = Instantiate(prefabRock) as GameObject;
+            rock.transform.position = worldLocation;
+
+            SpriteRenderer spriteRenderer = rock.GetComponent<SpriteRenderer>();
+
+            rocks = Resources.LoadAll("Sprites", typeof(Sprite));
+
+            Sprite sprite = (Sprite)rocks[Random.Range(0, rocks.Length)];
+            spriteRenderer.sprite = sprite;
+        }
+
+    }
+
+}
