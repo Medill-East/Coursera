83809595e426f103ddb462d4fe51da80e57991c9
Medill-East <NEMailLHD@163.com>
2018-10-17 00:13:36 +0800

    week1 wackybreakout
    until step4 20181017

---
+++ Intermediate Object-Oriented Programming for Unity Games/Week 1/WackyBreakout/Assets/Scripts/Gameplay/Paddle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class for paddle
+/// </summary>
+public class Paddle : MonoBehaviour {
+
+    Rigidbody2D rigidbody2D;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            rigidbody2D.MovePosition(rigidbody2D.position - 0.0001f * new Vector2(ConfigurationUtils.PaddleMoveUnitsPerSecond / Time.fixedDeltaTime, 0));
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            rigidbody2D.MovePosition(rigidbody2D.position + 0.0001f * new Vector2(ConfigurationUtils.PaddleMoveUnitsPerSecond / Time.fixedDeltaTime, 0));
+        }
+    }
+}
