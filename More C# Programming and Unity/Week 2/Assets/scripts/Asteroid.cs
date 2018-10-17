d8ab70e9dd3b5cc0da70afc3650a5a74747a3627
Medill-East <NEMailLHD@163.com>
2018-10-18 01:22:50 +0800

    More Week2
    201809121908

---
+++ More C# Programming and Unity/Week 2/Assets/scripts/Asteroid.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An asteroid
+/// </summary>
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    Sprite asteroidSprite0;
+    [SerializeField]
+    Sprite asteroidSprite1;
+    [SerializeField]
+    Sprite asteroidSprite2;
+
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start()
+	{
+
+
+        // set random sprite for asteroid
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        int spriteNumber = Random.Range(0, 3);
+        if (spriteNumber < 1)
+        {
+            spriteRenderer.sprite = asteroidSprite0;
+        }
+        else if (spriteNumber < 2)
+        {
+            spriteRenderer.sprite = asteroidSprite1;
+        }
+        else
+        {
+            spriteRenderer.sprite = asteroidSprite2;
+        }
+	}
+
+    public void Initialize(Direction dir,Vector3 location)
+    {
+        if (dir == Direction.Up)
+        {
+            transform.position = location;
+            // apply impulse force to get game object moving
+            const float MinImpulseForce = 3f;
+            const float MaxImpulseForce = 5f;
+            float angle = Random.Range(75 * Mathf.Deg2Rad, 75 * Mathf.Deg2Rad + 30 * Mathf.Deg2Rad);
+            Vector2 moveDirection = new Vector2(
+                Mathf.Cos(angle), Mathf.Sin(angle));
+            float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+            GetComponent<Rigidbody2D>().AddForce(
+                moveDirection * magnitude,
+                ForceMode2D.Impulse);
+            
+        }
+
+        if (dir == Direction.Left)
+        {
+            transform.position = location;
+
+            // apply impulse force to get game object moving
+            const float MinImpulseForce = 3f;
+            const float MaxImpulseForce = 5f;
+            float angle = Random.Range(165 * Mathf.Deg2Rad, 165 * Mathf.Deg2Rad + 30 * Mathf.Deg2Rad);
+            Vector2 moveDirection = new Vector2(
+                Mathf.Cos(angle), Mathf.Sin(angle));
+            float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+            GetComponent<Rigidbody2D>().AddForce(
+                moveDirection * magnitude,
+                ForceMode2D.Impulse);
+        }
+
+        if (dir == Direction.Down)
+        {
+            transform.position = location;
+
+            // apply impulse force to get game object moving
+            const float MinImpulseForce = 3f;
+            const float MaxImpulseForce = 5f;
+            float angle = Random.Range(255 * Mathf.Deg2Rad, 255 * Mathf.Deg2Rad + 30 * Mathf.Deg2Rad);
+            Vector2 moveDirection = new Vector2(
+                Mathf.Cos(angle), Mathf.Sin(angle));
+            float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+            GetComponent<Rigidbody2D>().AddForce(
+                moveDirection * magnitude,
+                ForceMode2D.Impulse);
+        }
+
+        if (dir == Direction.Right)
+        {
+            transform.position = location;
+
+            // apply impulse force to get game object moving
+            const float MinImpulseForce = 3f;
+            const float MaxImpulseForce = 5f;
+            float angle = Random.Range(-15 * Mathf.Deg2Rad, -15 * Mathf.Deg2Rad + 30 * Mathf.Deg2Rad);
+            Vector2 moveDirection = new Vector2(
+                Mathf.Cos(angle), Mathf.Sin(angle));
+            float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+            GetComponent<Rigidbody2D>().AddForce(
+                moveDirection * magnitude,
+                ForceMode2D.Impulse);
+        }
+
+    }
+}
---
+++ More C# Programming and Unity/Week 2/Assets/scripts/AsteroidSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// spawner for asteroids
+/// </summary>
+public class AsteroidSpawner : MonoBehaviour {
+
+    [SerializeField]
+    GameObject prefabAsteroid;
+    Asteroid scriptAsteroid;
+    float colliderRadius;
+
+	// Use this for initialization
+	void Start () {
+        //GameObject cloneAsteroid = Instantiate(prefabAsteroid, Vector3.zero, Quaternion.identity);
+        //cloneAsteroid.GetComponent<Asteroid>().Initialize(Direction.Up,Vector3.zero);
+        //colliderRadius = cloneAsteroid.GetComponent<CircleCollider2D>().radius;
+        //Destroy(cloneAsteroid);
+        colliderRadius = prefabAsteroid.GetComponent<CircleCollider2D>().radius;
+        Debug.Log(colliderRadius);
+
+
+        Vector3 rightPosition = new Vector3(ScreenUtils.ScreenRight + 2 * colliderRadius, 0, 0);
+        GameObject moveLeftAsteroid = Instantiate(prefabAsteroid,rightPosition, Quaternion.identity);
+        moveLeftAsteroid.GetComponent<Asteroid>().Initialize(Direction.Left, rightPosition);
+
+        Vector3 topPosition = new Vector3(0, ScreenUtils.ScreenTop + 2 * colliderRadius, 0);
+        GameObject moveDownAsteroid = Instantiate(prefabAsteroid, topPosition, Quaternion.identity);
+        moveDownAsteroid.GetComponent<Asteroid>().Initialize(Direction.Down, topPosition);
+
+        Vector3 leftPosition = new Vector3(ScreenUtils.ScreenLeft - 2 * colliderRadius, 0, 0);
+        GameObject moveRightAsteroid = Instantiate(prefabAsteroid, leftPosition, Quaternion.identity);
+        moveRightAsteroid.GetComponent<Asteroid>().Initialize(Direction.Right, leftPosition);
+
+        Vector3 bottomPosition = new Vector3(0, ScreenUtils.ScreenBottom - 2 * colliderRadius, 0);
+        GameObject moveUpAsteroid = Instantiate(prefabAsteroid, bottomPosition, Quaternion.identity);
+        moveUpAsteroid.GetComponent<Asteroid>().Initialize(Direction.Up, bottomPosition);
+
+    }
+	
+}
